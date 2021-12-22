c029ddd581998dbdab9087bac2d94ca0804c4ce4
alex_0ff <coulay37@gmail.com>
2021-12-22 18:40:46 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 const twitch = require('../dist/index');
-const { CLIENT_ID, CLIENT_SECRET, TOKEN } = require("./config.json");
+const { CLIENT_ID, CLIENT_SECRET } = require("./config.json");
 
 const SCOPES = "user:read:email";
 
 
 async function script() {
-    /*const request = await twitch.getToken(CLIENT_ID, CLIENT_SECRET, SCOPES);
+    const request = await twitch.getToken(CLIENT_ID, CLIENT_SECRET, SCOPES);
 
-    const token = request.access_token;*/
+    const token = request.access_token;
 
-    const client = new twitch.default(CLIENT_ID, TOKEN);
+    const client = new twitch.default(CLIENT_ID, token);
 
     const user_id = await client.userInfo("alex_off");
 
